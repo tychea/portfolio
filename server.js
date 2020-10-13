11dc9fecf734651f1e9ba956bde8ce3233bf0848
tychea <steventychea@gmail.com>
2020-10-13 22:23:28 +0700

    initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express();
+
+//import authentication Routes
+const authRoutes = require("./routes/auth");
+
+//Routes Middleware
+app.use('/api/user',authRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World! How Are You Steven, Testing')
+  })
+  
+app.listen(3000,()=>{
+    console.log("Server is Running on Port 3000");
+})
